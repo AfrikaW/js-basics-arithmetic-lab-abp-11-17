02db519da9e9e60fa7fca5f6528071d8f4a7abbe
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-11 03:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const newID= oldID + 1000000000;
 const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.random()*20;
 const randomInteger = Math.floor(randomNumber);
-const randomUserID =Math.floor(randomNumber()*1000000000+1);
+const randomUserID =Math.floor(Math.random()*1000000000+1);
